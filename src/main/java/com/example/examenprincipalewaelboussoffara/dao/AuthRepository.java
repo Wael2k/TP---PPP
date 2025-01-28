b64a7b621ed4d7648512735326fd5e188052b8f8
waelboussoffara@gmail.com <waelboussoffara@gmail.com>
2025-01-28 22:17:11 +0100

    FIX

--- src/main/java/com/example/examenprincipalewaelboussoffara/dao/AuthRepository.java
+++ src/main/java/com/example/examenprincipalewaelboussoffara/dao/AuthRepository.java
@@ -1,8 +1,7 @@
 package com.example.examenprincipalewaelboussoffara.dao;
 
 import com.example.examenprincipalewaelboussoffara.dao.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.JpaRepository;g
 import java.util.Optional;
 
 public interface AuthRepository extends JpaRepository<User,Long> {
