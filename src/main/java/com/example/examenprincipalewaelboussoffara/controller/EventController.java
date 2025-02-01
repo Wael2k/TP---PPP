eabad2e67d8bc0ca3283ac99ec17102a130ebe13
waelboussoffara@gmail.com <waelboussoffara@gmail.com>
2025-02-01 09:27:33 +0100

    new pr

--- src/main/java/com/example/examenprincipalewaelboussoffara/controller/EventController.java
+++ src/main/java/com/example/examenprincipalewaelboussoffara/controller/EventController.java
@@ -27,7 +27,7 @@ public class EventController {
     @GetMapping("test")
     public ResponseEntity<String> test() {
 
-        return ResponseEntity.ok("Hello World");
+        return ResponseEntity.ok("Hello World i'm here today");
     }
     @PutMapping("update")
     public ResponseEntity<EventResponseDto> update(@RequestBody EventUpdateRequestDto eventUpdateRequestDto) {
